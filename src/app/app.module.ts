86997c00c87c4e3413f8bae3910e94af88f9641a
Florian Feka <florianfeka05@gmail.com>
2020-04-26 12:41:08 +0200

    add lecture list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
+import { LectureListComponent } from './lecture-list/lecture-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LectureListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lecture-list/lecture-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lecture-list',
+  templateUrl: './lecture-list.component.html',
+  styleUrls: ['./lecture-list.component.css']
+})
+export class LectureListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

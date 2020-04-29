24d2c6d392b6f54543effd26a68bf48c92a58840
FlorianFeka <florianfeka05@gmail.com>
2020-04-29 22:13:01 +0200

    add lecture detail component

---
+++ src/app/module/lecture/page/lecture-detail/lecture-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lecture-lecture-detail',
+  templateUrl: './lecture-detail.component.html',
+  styleUrls: ['./lecture-detail.component.css']
+})
+export class LectureDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

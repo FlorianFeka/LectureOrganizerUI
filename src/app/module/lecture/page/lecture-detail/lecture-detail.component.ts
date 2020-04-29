97ddc5c777571c16878c7c8517a7b6a10d7752c9
FlorianFeka <florianfeka05@gmail.com>
2020-04-29 23:22:05 +0200

    add navigating to detail view from list view

--- src/app/module/lecture/page/lecture-detail/lecture-detail.component.ts
+++ src/app/module/lecture/page/lecture-detail/lecture-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+
+import { Lecture } from '../../../../../api/model/lecture';
+import { GetOneLecture } from '../../../../store/lecture.actions';
+import { LectureState } from '../../../../store/lecture.state';
 
 @Component({
   selector: 'lecture-lecture-detail',
@@ -6,10 +13,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lecture-detail.component.css']
 })
 export class LectureDetailComponent implements OnInit {
+  lecture$: Observable<Lecture>;
 
-  constructor() { }
+  constructor(private store: Store, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const lectureId = +params.get('id');
+      this.store.dispatch(new GetOneLecture(lectureId));
+    });
+    this.lecture$ = this.store.select(LectureState.lecture);
   }
-
 }

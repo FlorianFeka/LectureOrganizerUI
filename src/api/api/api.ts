544dc487007c9d5535bd11231a7241b86df54ca5
Florian Feka <47530143+FlorianFeka@users.noreply.github.com>
2020-10-27 17:51:42 +0100

    generate new api

--- src/api/api/api.ts
+++ src/api/api/api.ts
@@ -1,5 +1,5 @@
-export * from './lectures.service';
-import { LecturesService } from './lectures.service';
-export * from './users.service';
-import { UsersService } from './users.service';
-export const APIS = [LecturesService, UsersService];
+export * from './lecture.service';
+import { LectureService } from './lecture.service';
+export * from './user.service';
+import { UserService } from './user.service';
+export const APIS = [LectureService, UserService];
